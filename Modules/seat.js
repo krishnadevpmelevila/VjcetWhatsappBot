3daff7304b5d8968fcfe9c8db3cf5d116090124f
Krishnadev P Melevila <krishnadev@kali>
2022-08-26 21:04:37 +0530

    Added all the best

--- Modules/seat.js
+++ Modules/seat.js
@@ -4,6 +4,7 @@ const { MessageMedia } = require('whatsapp-web.js');
 
 function seat(message,client,media) {
     try{
+        client.sendMessage(chatId = message.from, 'All the best for your exam Makkkalleee!!');
         client.sendMessage(chatId = message.from, media);
     }catch(err){
         console.log(err);
