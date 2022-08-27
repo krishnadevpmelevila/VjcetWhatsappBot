fa658c53a425a68fdc309eaf6c641bd293b5dfe8
Krishnadev P Melevila <krishnadev@kali>
2022-08-27 16:25:07 +0530

    Bug fix

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ client.on('message', async message => {
         }
     }
     // quotes
-    if (message.body.toLowerCase() == "!quotes" || message.body.toLowerCase() == "! quotes" || message.body.toLowerCase=="!quote" || message.body.toLowerCase=="! quote") {
+    if (message.body.toLowerCase() == "!quotes" || message.body.toLowerCase() == "! quotes" || message.body.toLowerCase()=="!quote" || message.body.toLowerCase()=="! quote") {
         quotes(message)
     }
 
